20aece625600151f47a71a1687fe5900d8642c84
dvistr <dvistr@gmail.com>
2024-08-26 12:49:40 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ sekond=(input('Введите число: '))
 third=(input('Введите число: '))
 if first == sekond and sekond == third:
     print('3')
-if first == sekond or sekond==third or first==third:
-    print ('0')
-if not first == sekond and not sekond == third:
-    print('2')
+elif first == sekond or sekond==third or first==third:
+    print ('2')
+elif not first == sekond and not sekond == third:
+    print('0')
