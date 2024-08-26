fc831274dbedc28981f670c99fdf6747c21b5206
dvistr <dvistr@gmail.com>
2024-08-26 13:14:35 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -5,5 +5,7 @@ if first == sekond and sekond == third:
     print('3')
 elif first == sekond or sekond==third or first==third:
     print ('2')
-elif not first == sekond and not sekond == third:
+#elif not first == sekond and not sekond == third:
+    print('0')
+else:
     print('0')
